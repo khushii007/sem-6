113cd9cb2ed52fa11e215d7b6752a93ca7e1b6ab
Khushi Israni <113411572+khushii007@users.noreply.github.com>
2024-01-13 11:23:47 +0530

    lab 1 examples done

---
+++ DSL/lab1/examples/ex1.py
@@ -0,0 +1,17 @@
+counter = 100 # An integer assignment
+miles = 1000.0 # A floating point
+name = "John" # A string
+print (counter)
+print (miles) 
+print (name)
+
+a = 5 # integer assignment
+b= 4.56 #floating point assignment #mathematical operations with scalar variables print (5*a), would give the result 25
+print (a/2) #would give the result 2.5
+print(a**2) #is the power (squaring operation) would give the result 25
+
+str = 'Hello World!'
+print (str) # Prints complete string
+print (str[0]) # Prints first character of the string
+print (str[2:5]) # Prints characters starting from 3rd to 5th print (str[2:]) # Prints string starting from 3rd character print (str * 2) # Prints string two times
+print (str + "TEST") # Prints concatenated string
---
+++ DSL/lab1/examples/ex2.py
@@ -0,0 +1,16 @@
+var1 = 'Hello World!'
+print ("Updated String :", var1[:6] + 'Python')
+
+print( "My name is %s and weight is %d kg!" % ('Abay', 55))
+
+str = "this is string example wow!!!";
+print (str.capitalize())
+
+str = "this is string example ...wow!!!";
+s_count = str.count('s')
+print(s_count)
+
+str = "THIS IS STRING EXAMPLE...WOW!!!"; print (str.lower())
+
+str = "this is string example ...wow!!! this is really string";
+print (str.replace("is", "was"))
